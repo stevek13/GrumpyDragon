5e9a1e49cf1b6c2ada9bdabd5146eb9a4ff3eeaa
Steve Kite <skite1@hotmail.com>
2017-09-01 15:33:14 -0700

    Lesson 37 completed

--- GrumpyDragon/GameScene.swift
+++ GrumpyDragon/GameScene.swift
@@ -9,81 +9,148 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+struct Contact {
+    var scene:UInt32 { return 1<<0 }
+    var obstacle:UInt32 { return 1<<1 }
+    var dragon:UInt32 { return 1<<2 }
+    var score:UInt32 { return 1<<3 }
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+}
+struct AssetName {
+    var columnObstacle = "ColumnObstacle"
+    var ground = "Ground"
+    var grumpyDragon = "GrumpyDragon"
+}
+class GameScene: SKScene, SKPhysicsContactDelegate{
+    
+    var grumpyDragonSprite : SKSpriteNode!
+    var columnSprite : SKSpriteNode!
+    var topColumnSprite : SKSpriteNode!
+    var bottomColumnSprite : SKSpriteNode!
+    var newTopColumnSprite : SKSpriteNode!
+    var newBottomColumnSprite : SKSpriteNode!
+    var columnArray = [SKSpriteNode]()
+    let screenSize = UIScreen.main.bounds.size
+    let gap:CGFloat = 250
+    var frameCount : TimeInterval = 0.0
+    var lastUpdateTime : TimeInterval = 0.0
+    var flyingDragonTexture = [SKTexture]()
+    let flapTime:TimeInterval = 0.1
+    
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        physics()               //setup  the physics on the screen
+        addGround()             //draw the ground
+        createDragonSprite()    //draw the dragon
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        topColumnSprite = createColumn()
+        bottomColumnSprite = createColumn()
+        columnArray = [topColumnSprite, bottomColumnSprite]
+    }
+    func spawn()
+    {
+        let randomY = CGFloat(arc4random_uniform(UInt32(screenSize.height/3)))
+        bottomColumnSprite = columnArray[1].copy() as! SKSpriteNode
+        topColumnSprite  = columnArray[0].copy() as! SKSpriteNode
+        bottomColumnSprite.position = CGPoint(x: screenSize.width + bottomColumnSprite.size.width , y: randomY)
+        topColumnSprite.position = CGPoint(x:bottomColumnSprite.position.x, y:bottomColumnSprite.size.height + bottomColumnSprite.position.y + gap)
+        addChild(topColumnSprite)
+        addChild(bottomColumnSprite)
+        print(randomY)
+    }
+    
+    
+    func createColumn() -> SKSpriteNode{
+        let column = SKSpriteNode(imageNamed : AssetName().columnObstacle)
+        column.physicsBody = SKPhysicsBody(edgeLoopFrom: column.frame)
+        column.physicsBody?.affectedByGravity = false
+        column.physicsBody?.categoryBitMask = Contact().obstacle
+        column.name = AssetName().columnObstacle
+        column.physicsBody?.collisionBitMask = 0x0
+        column.physicsBody?.contactTestBitMask = 0x0
+        return column
+    }
+    func createDragonSprite() {
+        let animatedAtlas = SKTextureAtlas(named: "GrumpyDragon")
+        var frames = [SKTexture]()
+        let numImages = animatedAtlas.textureNames.count
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        for i in 1...numImages/3 {
+            let textureName = "grumpyDragon\(i)"
+            frames.append(animatedAtlas.textureNamed(textureName))
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
         }
+        let temp = frames.first
+        grumpyDragonSprite = SKSpriteNode(texture: temp)
+        grumpyDragonSprite.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        grumpyDragonSprite.setScale(0.3)
+        grumpyDragonSprite.physicsBody = SKPhysicsBody(circleOfRadius: grumpyDragonSprite.size.height/3)
+        grumpyDragonSprite.physicsBody?.categoryBitMask = Contact().dragon
+        grumpyDragonSprite.physicsBody?.collisionBitMask = Contact().scene
+        grumpyDragonSprite.physicsBody?.contactTestBitMask = Contact().scene | Contact().obstacle | Contact().score
+        
+        addChild(grumpyDragonSprite)
+        
+        let animation = SKAction.animate(with: frames, timePerFrame: flapTime)
+        grumpyDragonSprite.run(SKAction.repeatForever(animation))
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func physics() {
+        self.physicsWorld.contactDelegate = self
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -10 )
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.physicsBody?.affectedByGravity = false
+        self.physicsBody?.categoryBitMask = Contact().scene
+        self.physicsBody?.collisionBitMask = 0x0
+        self.physicsBody?.contactTestBitMask = 0x0
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    func addGround() {
+        let ground = SKSpriteNode(imageNamed: "Ground")
+        ground.anchorPoint = CGPoint.zero
+        ground.physicsBody = SKPhysicsBody(edgeLoopFrom: ground.frame)
+        ground.physicsBody?.affectedByGravity = false
+        ground.physicsBody?.categoryBitMask = Contact().scene
+        ground.physicsBody?.collisionBitMask = 0x0
+        ground.physicsBody?.contactTestBitMask = 0x0
+        ground.size = CGSize(width: self.frame.width, height: ground.frame.height)
+        ground.zPosition = -1   //put behind everything
+        addChild(ground)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        grumpyDragonSprite.physicsBody?.velocity = CGVector.zero
+        let pushUp = CGVector(dx: 0, dy: 50)
+        grumpyDragonSprite.physicsBody?.applyImpulse(pushUp)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
+        let delta = currentTime - lastUpdateTime
+        lastUpdateTime = currentTime
+        
+        frameCount += delta
+        
+        if frameCount > 2
+        {
+            spawn()
+            frameCount = 0.0 // need to res
+        }
+        //4 means the time between columns being displayed
+        let movement = CGFloat(delta) * 60 * 4
+        
+        // TODO: create structs for the strings
+        
+        for node in self.children {
+            if node.name == AssetName().columnObstacle
+            {
+                if node.position.x < (0 - node.frame.width) {
+                    node.removeFromParent()
+                }else{
+                    node.position.x = node.position.x - movement
+                }
+            }
+        }
     }
 }
+
